e7d770f77373e093b2282ca27af87be5f4218c72
David <davidprgt@gmail.com>
2025-11-04 12:40:04 +0100

    github pages fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
 import Home from "./pages/Home.jsx";
 import Match from "./pages/Match.jsx";
@@ -10,22 +10,21 @@ import Ranking from "./pages/Ranking.jsx";
 import Compare from "./pages/Compare";
 import "./index.css";
 
-const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <App />,
-      children: [
-        { index: true, element: <Home /> },
-        { path: "partido/:id", element: <Match /> },
-        { path: "jugadores", element: <Players /> },
-        { path: "jugador/:name", element: <Player /> },
-        { path: "ranking", element: <Ranking /> },
-        { path: "compare", element: <Compare /> }
-      ],
-    },
-  ],
-  { basename: import.meta.env.BASE_URL } // "/" en dev, "/gazalbide-stats/" en GH Pages
-);
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "partido/:id", element: <Match /> },
+      { path: "jugadores", element: <Players /> },
+      { path: "jugador/:name", element: <Player /> },
+      { path: "ranking", element: <Ranking /> },
+      { path: "compare", element: <Compare /> },
+    ],
+  },
+]);
 
-createRoot(document.getElementById("root")).render(<RouterProvider router={router} />);
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
