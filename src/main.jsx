14915ae726ca4065dc99de480c4b594e79d50a5c
DavidPerez3 <113512357+DavidPerez3@users.noreply.github.com>
2026-08-15 22:35:40 +0100

    Redirect old Le Gazal route into Fantasy

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { createHashRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, Navigate, RouterProvider } from "react-router-dom";
 
 import App from "./App.jsx";
 import Home from "./pages/Home.jsx";
@@ -43,6 +43,7 @@ const router = createHashRouter([
       { path: "jugador/:name", element: <PlayerProfilePage /> },
       { path: "ranking", element: <Ranking /> },
       { path: "compare", element: <Compare /> },
+      { path: "le-gazal", element: <Navigate to="/fantasy/le-gazal" replace /> },
 
       { path: "login", element: <AuthPage /> },
       { path: "forgot-password", element: <ForgotPassword /> },
