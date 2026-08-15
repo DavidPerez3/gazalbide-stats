d7c8d1fd154007473a194245f7c9bb7eafbffba2
DavidPerez3 <113512357+DavidPerez3@users.noreply.github.com>
2026-08-16 00:02:42 +0100

    Mount Live Stats action history panel

--- src/pages/LiveStatsWithStaffPage.jsx
+++ src/pages/LiveStatsWithStaffPage.jsx
@@ -1,10 +1,12 @@
 import LiveStatsPage from "./LiveStatsPage.jsx";
 import LiveStaffDisciplinePanel from "../features/live-stats/LiveStaffDisciplinePanel.jsx";
+import LiveActionHistoryPanel from "../features/live-stats/LiveActionHistoryPanel.jsx";
 
 export default function LiveStatsWithStaffPage() {
   return (
     <>
       <LiveStatsPage />
+      <LiveActionHistoryPanel />
       <LiveStaffDisciplinePanel />
     </>
   );
